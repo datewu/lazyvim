df7c92992eda22328078e2ec630ce98300949029
datewu <hihahajun@gmail.com>
2024-07-06 11:57:08 +0800

    add sg.vim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -24,3 +24,15 @@ vim.keymap.set("n", "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)")
 vim.keymap.set("n", "=p", "<Plug>(YankyPutAfterFilter)")
 vim.keymap.set("n", "=P", "<Plug>(YankyPutBeforeFilter)")
 -- keymap for yanky.nvim  ends here
+
+-- keymap for sg
+-- Example mapping for doing searches from within neovim (may change) using telescope.
+-- (requires telescope.nvim to be installed)
+-- nnoremap <space>ss <cmd>lua require('sg.extensions.telescope').fuzzy_search_results()<CR>
+-- vim.api.nvim_set_keymap(
+--   "n",
+--   "<space>ss",
+--   '<cmd>lua require("sg.extensions.telescope").fuzzy_search_results()<CR>',
+--   { noremap = true, silent = true }
+-- )
+-- keymap for sg ends here
